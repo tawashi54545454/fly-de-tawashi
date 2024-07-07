2735cd264ca24966a08798d258b88ec138d6535d
tawashi <github@tawashi.jp>
2024-07-08 02:20:35 +0900

    adjust trailing thrash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,11 @@ import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
-import rehypeKatex from "rehype-katex";
-import remarkMath from "remark-math";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [
-      [
-        rehypeKatex,
-        {
-          // Katex plugin options
-        },
-      ],
-    ],
-  },
   image: {
     service: passthroughImageService(),
   },
@@ -35,7 +22,7 @@ export default defineConfig({
       },
     }),
   ],
-  trailingSlash: "never",
+  trailingSlash: "ignore",
 });
 // https://astro.build/config
 // export default defineConfig({});
